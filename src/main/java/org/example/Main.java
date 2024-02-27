e563a6283850a87a3b53dc16d35f8d93aa1cc633
Sahil-2002 <sahila.khandekar@gmail.com>
2024-02-27 18:17:22 +0530

    find cheapest hotel for given date range

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,49 +3,73 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
-class Hotel{
-    private String name ;
+class Hotel {
+    private String name;
     private Map<String, Integer> regularrates;
-    public Hotel(String name){
-        this.name= name;
-this.regularrates= new HashMap<>();
+
+    public Hotel(String name) {
+        this.name = name;
+        this.regularrates = new HashMap<>();
 
     }
-    public void setRegularrates(String dayytpe, int rate ){
-        regularrates.put(dayytpe,rate);
+
+    public void setRegularrates(String dayytpe, int rate) {
+        regularrates.put(dayytpe, rate);
 
     }
-    public String getname(){
+
+    public String getname() {
         return name;
     }
-    public Map<String, Integer> getRegularrates(){
+
+    public Map<String, Integer> getRegularrates() {
         return regularrates;
 
     }
 
-
+    public int getTotalRate(String dayType, int numDays) {
+        return regularrates.getOrDefault(dayType, 0) * numDays;
+    }
 }
-
-
-
-
 
 public class Main {
 
     Map<String, Hotel> hotels;
-    public Main(){
-        this.hotels=new HashMap<>();
+
+    public Main() {
+        this.hotels = new HashMap<>();
 
     }
-    public void addhotel(String name){
-        hotels.put(name,new Hotel(name));
+
+    public void addhotel(String name) {
+        hotels.put(name, new Hotel(name));
     }
-public void setregularrates(String hotelname , String daytype , int rate){
-     Hotel  hotel = hotels.get(hotelname);
-     if(hotel!=null){
-         hotel.setRegularrates(daytype,rate);
-     }
-}
+
+    public void setregularrates(String hotelname, String daytype, int rate) {
+        Hotel hotel = hotels.get(hotelname);
+        if (hotel != null) {
+            hotel.setRegularrates(daytype, rate);
+        } else {
+            System.out.println("hotel " + hotelname + " not found ");
+        }
+    }
+
+    public String findCheapestHotel(String startDate, String endDate) {
+        int minTotalRate = Integer.MAX_VALUE;
+
+        String cheapestHotel = "";
+
+        for (Hotel hotel : hotels.values()) {
+            int totalRate = hotel.getTotalRate("Weekday", 2);
+            if (totalRate < minTotalRate) {
+                minTotalRate = totalRate;
+                cheapestHotel = hotel.getname();
+            }
+        }
+
+        return cheapestHotel + " total rates " + minTotalRate;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation Program ");
 
@@ -54,9 +78,9 @@ public void setregularrates(String hotelname , String daytype , int rate){
         sc.addhotel("Bridgewood");
         sc.addhotel("Ridgewood");
 
-        sc.setregularrates("Lakewood","Weekday",110);
-        sc.setregularrates("Bridgewood","Weekday",160);
-        sc.setregularrates("Ridgewood","Weekday", 220);
+        sc.setregularrates("Lakewood", "Weekday", 110);
+        sc.setregularrates("Bridgewood", "Weekday", 160);
+        sc.setregularrates("Ridgewood", "Weekday", 220);
 
 
         Hotel lakewood = sc.hotels.get("Lakewood");
@@ -64,6 +88,11 @@ public void setregularrates(String hotelname , String daytype , int rate){
         if (lakewood != null) {
             System.out.println("Lakewood Rates for Regular Customers: " + lakewood.getRegularrates());
         }
+        String startDate = "10sep2020";
+        String endDate = "11sep2020";
+        String hotel = sc.findCheapestHotel(startDate, endDate);
+        System.out.println(hotel);
+
 
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -10,14 +10,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MainTest {
 
     @Test
-    public void addhotel(){
+    public void addhotel() {
         Main sc = new Main();
         sc.addhotel("Lakewood");
-        sc.setregularrates("Lakewood","Weekday",110);
+        sc.setregularrates("Lakewood", "Weekday", 110);
         Hotel lakewood = sc.hotels.get("Lakewood");
-        assertEquals(110,lakewood.getRegularrates().get("Weekday"));
+        assertEquals(110, lakewood.getRegularrates().get("Weekday"));
 
 
+    }
+
+    @Test
+    public void findcheapesthotel() {
+        Main sc = new Main();
+        sc.addhotel("Lakewood");
+        sc.addhotel("Bridgewood");
+        sc.addhotel("Ridgewood");
+
+        sc.setregularrates("Lakewood", "Weekday", 110);
+        sc.setregularrates("Bridgewood", "Weekday", 160);
+        sc.setregularrates("Ridgewood", "Weekday", 220);
+        String startDate = "10sep2020";
+        String endDate = "11sep2020";
+        String hotel = sc.findCheapestHotel(startDate, endDate);
+        assertEquals("Lakewood total rates 220", hotel);
     }
 
 }
